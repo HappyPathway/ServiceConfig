fdbb0a4fb65aafaeaacdbc2aac75407879537b34
Dave Arnold <dja+djaboxx@sonic.net>
2018-05-06 13:26:49 -0700

    Update github.tf

--- github.tf
+++ github.tf
@@ -27,14 +27,14 @@ module "dev_repo_team" {
 }
 
 resource "github_team_membership" "dev_team_membership" {
-  team_id  = "${module.dev_team.id}"
+  team_id  = "${module.dev_team.team_id}"
   username = "${elements(var.devs, count.index)}"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "admin_team_membership" {
   count = "${length(var.admins)}"
-  team_id  = "${module.admin_team.id}"
+  team_id  = "${module.admin_team.team_id}"
   username = "${elements(var.admins, count.index)}"
   role     = "maintainer"
 }
