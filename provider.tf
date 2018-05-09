e40abeda85904f396609a16560e9412ba5e511bc
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-09 15:43:20 -0700

    fixing provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 # Configure the GitHub Provider
 provider "github" {
-  token        = "${data.vault_generic_secret.rundeck_auth.data.github_token}"
-  organization = "${data.vault_generic_secret.rundeck_auth.data.github_organization}"
+  token        = "${data.vault_generic_secret.credentials.data.github_token}"
+  organization = "${data.vault_generic_secret.credentials.data.github_organization}"
 }
 
 # Configure the PagerDuty provider
