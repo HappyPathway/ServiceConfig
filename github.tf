60b30ee12656589405e32ddb468bdaa54a6f988c
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-09 15:01:51 -0700

    no sources for modules? strange it worked

--- github.tf
+++ github.tf
@@ -1,34 +1,41 @@
 module "admin_team" {
+  source = "git@github.com:HappyPathway/GitHubTeam.git"
   team = "${var.repo}Admin"
   description = "${var.repo} Administrators"
   privacy = "secret"
 }
 
 module "admin_repo_team" {
+  source = "git@github.com:HappyPathway/GitHubTeamRepo.git"
   team_id = "${module.admin_team.id}"
   repository = "${var.repo}"
   permission = "admin"
 }
 
 module "dev_team" {
+  source = "git@github.com:HappyPathway/GitHubTeam.git"
   team = "${var.repo}Dev"
   description = "${var.repo} Developers"
   privacy = "secret"
 }
 
 module "dev_repo_team" {
+  source = "git@github.com:HappyPathway/GitHubTeamRepo.git"
   team_id = "${module.dev_team.id}"
   repository = "${var.repo}"
   permission = "push"
 }
 
 resource "github_team_membership" "dev_team_membership" {
+  source = "git@github.com:HappyPathway/GitHubTeamMembership.git"
+  count = "${length(var.admins)}"
   team_id  = "${module.dev_team.id}"
   username = "${elements(var.devs, count.index)}"
   role     = "maintainer"
 }
 
 resource "github_team_membership" "admin_team_membership" {
+  source = "git@github.com:HappyPathway/GitHubTeamMembership.git"
   count = "${length(var.admins)}"
   team_id  = "${module.admin_team.id}"
   username = "${elements(var.admins, count.index)}"
