5abbf8ba45a56288897c0f12339625872bbad8c8
Dave Arnold <darnold@l-sfh3k1htdd-m.gap.com>
2018-05-09 15:37:29 -0700

    getting values from vault

---
+++ lookups.tf
@@ -0,0 +1,13 @@
+provider "vault" {
+  # It is strongly recommended to configure this provider through the
+  # environment variables described above, so that each user can have
+  # separate credentials set in the environment.
+  #
+  # This will default to using $VAULT_ADDR
+  # But can be set explicitly
+  address = "http://consul.ops.happypathway.com:8200"
+}
+
+data "vault_generic_secret" "credentials" {
+  path = "secret/credentials"
+}
---
+++ provider.tf
@@ -0,0 +1,10 @@
+# Configure the GitHub Provider
+provider "github" {
+  token        = "${data.vault_generic_secret.rundeck_auth.data.github_token}"
+  organization = "${data.vault_generic_secret.rundeck_auth.data.github_organization}"
+}
+
+# Configure the PagerDuty provider
+provider "pagerduty" {
+  token = "${data.vault_generic_secret.rundeck_auth.data.pagerduty_token}"
+}
